4829953cadfce56c2375f9b805841f7d63b6e116
Luis <lfcorreacardenas@gmail.com>
2026-03-09 11:21:16 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -134,7 +134,7 @@ try:
 
     # --- SECCIÓN DE GRÁFICOS ---
     st.write("---")
-    g1, g2 = st.columns([1, 1])
+    g1, g2 = st.columns([0.8, 1])
     with g1:
         st.subheader("📊 Volumen por Cliente")
         data_bar = df['Cliente'].value_counts().reset_index().head(10)
@@ -177,6 +177,7 @@ try:
 
 except Exception as e:
     st.error(f"Error de sistema: {e}")
+
 
 
 
